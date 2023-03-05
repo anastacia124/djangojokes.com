abd583e2354efd84d5950c5f1a2cc40ed519a277
Anastacia <anastaciawebbs@gmail.com>
2023-03-05 13:26:11 -0500

    59 initial commit/ adding play2learn logo

--- jokes/admin.py
+++ jokes/admin.py
@@ -24,11 +24,18 @@ class JokeAdmin(DjangoJokesAdmin):
     list_filter = ['updated', 'category', 'tags']
     ordering = ['-updated']
     search_fields = ['question', 'answer']
+    
+    # Form Attributes
+    autocomplete_fields = ['tags', 'user']
+    radio_fields = { 'category': admin.HORIZONTAL}
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
-            return ('slug', 'created', 'updated')
+            return ('slug', 'created', 'updated', 'vote_summary')
         return ()
+    
+    def vote_summary(self, obj):
+        return f'{obj.num_votes} votes. Rating: {obj.rating}.'
 
 
 @admin.register(JokeVote)
@@ -46,7 +53,7 @@ class JokeVoteAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     model = Tag
     list_display = ['tag', 'created', 'updated']
-
+    search_fields = ['tag']
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
             return ('slug', 'created', 'updated')
