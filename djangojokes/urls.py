66baa9c3de4c0dbb1f044c502c446d12b903b1af
Anastacia <anastaciawebbs@gmail.com>
2023-03-05 00:49:10 -0500

    57 initial commit/ django debug toolbar

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -17,3 +17,9 @@ urlpatterns = [
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
