22863684d9c0e5bf549afd9071e83260f7d09ff1
Anastacia <anastaciawebbs@gmail.com>
2023-03-05 21:39:43 -0500

    Final commit

--- jokes/forms.py
+++ jokes/forms.py
@@ -5,16 +5,16 @@ from .models import Joke
 class JokeForm(ModelForm):
     class Meta:
         model = Joke
-        fields = ['question', 'answer', 'category', 'tags']
+        fields = ['question']
         widgets = {
             'question': Textarea(
                 attrs={'cols': 80, 'rows': 3, 'autofocus': True}
             ),
             'answer': Textarea(
-                attrs={'cols': 80, 'rows': 2, 'placeholder': 'Make it funny!'}
+                attrs={'cols': 80, 'rows': 2, 'placeholder': ' Add a Review!'}
             )
         }
         help_texts = {
-    'question': 'No dirty jokes please.',
+    'question': 'Add a nice review please.',
     'tags': 'Use Ctrl-click to select multiple tags.'
 }
